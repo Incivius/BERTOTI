577625241907676c27b3aab777eb056da1921bd5
Marcos Vinícius Malaquias <111438883+Incivius@users.noreply.github.com>
2023-03-19 22:37:44 -0300

    Add files via upload

---
+++ Fabricante.java
@@ -0,0 +1,21 @@
+/**
+ * Fabricante
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fabricante {
+    String fabricante;
+
+    public Fabricante(String fabricante){
+        this.fabricante = fabricante;
+    }
+    public void setFabricante(String fabricante){
+        this.fabricante = fabricante;
+    }
+    public String getFabricante( ){
+        return fabricante;
+
+    }
+
+    }
---
+++ Localização.java
@@ -0,0 +1,21 @@
+public class Localização {
+    int rua;
+    int andar;
+    
+    public Localização(int rua, int andar){
+        this.rua = rua;
+        this.andar = andar;
+    }
+    public int getRua(){
+        return rua;
+    }
+    public void setRua(int rua){
+        this.rua = rua;
+    }
+    public int getAndar(){
+        return andar;
+    }
+    public void setAndar(int andar){
+        this.andar = andar;
+    }
+}
